25ae2680bfdad0230a376c2fd253e06ca47f78dc
Kelvin Belo <kelvincrdzbl@gmail.com>
2025-11-15 17:24:27 -0300

    Atualiza CSRF_TRUSTED_ORIGINS para incluir URL de produção do Railway

--- mag_player/settings.py
+++ mag_player/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://server-production-6fe1.up.railway.app",
+]
 
 # Application definition
 
